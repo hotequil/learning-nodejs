5153c34348441666c81697fb02186abe422a4c6d
hotequil <hotequil@protonmail.com>
2021-11-29 20:45:20 -0300

    Done query.

--- class-ten/index.js
+++ class-ten/index.js
@@ -1,19 +1,28 @@
 const { MongoClient } = require('mongodb');
-const uri = `mongodb+srv://root:adalovelace@ada-lovelace-cluster.9jghq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const { URIMongoDBAdaLovelace } = require("../.env");
+const client = new MongoClient(URIMongoDBAdaLovelace, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect((error, connection) => {
      if(error) throw error;
 
      const database = connection.db('School');
      const value = { name: 'HTML', price: 25 };
+     const collection = database.collection('Courses');
 
-     database.collection('Courses')
-             .insertOne(value, (insertError, response) => {
-                  if(insertError) throw insertError;
+    // collection.insertOne(value, (insertError, response) => {
+    //     if(insertError) throw insertError;
+    //
+    //     console.log('Inserted data!', response);
+    //
+    //     connection.close();
+    // });
 
-                  console.log('Inserted data!', response);
+    collection.find({ name: 'Angular' })
+              .toArray((error, response) => {
+                  if(error) throw error;
+
+                  console.log(response);
 
                   connection.close();
-             });
+              });
 });
