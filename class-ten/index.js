692b46b36f0344ffda7870aae99e21bdcf6a86aa
hotequil <hotequil@protonmail.com>
2021-11-29 22:25:50 -0300

    Done aggregate.

--- class-ten/index.js
+++ class-ten/index.js
@@ -3,8 +3,39 @@ const { URIMongoDBAdaLovelace } = require("../.env");
 const client = new MongoClient(URIMongoDBAdaLovelace, { useNewUrlParser: true, useUnifiedTopology: true });
 const ASC = 1;
 const DESC = -1;
+const FIELD = 'slug';
 
-client.connect((error, connection) => {
+const frameworks = [
+    {
+        name: 'NodeJS',
+        [FIELD]: 'javascript'
+    },
+    {
+        name: 'Laravel',
+        [FIELD]: 'php'
+    },
+    {
+        name: 'Spring',
+        [FIELD]: 'java'
+    }
+];
+
+const languages = [
+    {
+        name: 'JavaScript',
+        [FIELD]: 'javascript'
+    },
+    {
+        name: 'PHP',
+        [FIELD]: 'php'
+    },
+    {
+        name: 'Java',
+        [FIELD]: 'java'
+    }
+];
+
+client.connect(async (error, connection) => {
      if(error) throw error;
 
      const database = connection.db('School');
@@ -28,16 +59,16 @@ client.connect((error, connection) => {
     //               connection.close();
     //           });
 
-    collection.find()
-              .sort({ name: DESC })
-              .limit(5)
-              .toArray((error, response) => {
-                  if(error) throw error;
-
-                  console.log(response, response.length);
-
-                  connection.close();
-              });
+    // collection.find()
+    //           .sort({ name: DESC })
+    //           .limit(5)
+    //           .toArray((error, response) => {
+    //               if(error) throw error;
+    //
+    //               console.log(response, response.length);
+    //
+    //               connection.close();
+    //           });
 
     // collection.deleteOne({ name: 'React' }, (error, response) => {
     //     if(error) throw error;
@@ -54,4 +85,29 @@ client.connect((error, connection) => {
     //
     //     connection.close();
     // });
+
+    const collectionFrameworks = database.collection('frameworks');
+    const collectionLanguagesName = 'languages';
+    const collectionLanguages = database.collection(collectionLanguagesName);
+
+    // await collectionFrameworks.insertMany(frameworks);
+    // await collectionLanguages.insertMany(languages);
+
+    collectionFrameworks.aggregate([
+                            {
+                                $lookup: {
+                                    from: collectionLanguagesName,
+                                    localField: FIELD,
+                                    foreignField: FIELD,
+                                    as: 'language'
+                                }
+                            }
+                        ])
+                        .toArray((error, response) => {
+                            if(error) throw error;
+
+                            console.log(JSON.stringify(response));
+
+                            connection.close();
+                        });
 });
